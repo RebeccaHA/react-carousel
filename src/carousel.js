178aa518cb6c9f4f349ee33627e2ba628784426e
RebeccaHA <rebeccahunt034@gmail.com>
2020-08-15 15:50:37 +0100

    added material icons into buttons, along with image src

--- src/carousel.js
+++ src/carousel.js
@@ -1,15 +1,16 @@
 import React from "react";
-import image_1 from "public/image_1.jpg";
-import image_2 from "public/image_2.jpg";
-import image_3 from "public/image_1.jpg";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 
 export default class Carousel extends React.Component {
+  const = [];
+
   render() {
     return (
       <div className="container">
-        <img src={image_1} alt="" />
+        <img src="public/image_1.jpg" alt="" />
+        <img src="public/image_2.jpg" alt="" />
+        <img src="public/image_3.jpg" alt="" />
         <button>
           <ArrowBackIosIcon />
         </button>
